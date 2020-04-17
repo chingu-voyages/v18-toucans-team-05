6fcb263eee266703ee9d89bf85ec9a02ee2e8de3
dev-grrl-japan <22501219+dev-grrl-japan@users.noreply.github.com>
2020-04-17 19:29:52 +0900

    Commit to Issues #36, #25, #20, #4, #26

---
+++ src/components/articleSummary.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+/**
+ * Article Summary 
+ * Displayed on Splash Page in articleList Container
+ * 
+ * Contains Title of Article, Author Avatar, Author, and Date Written
+ * 
+ */
+
+const ArticleSummary = () => {
+    return (
+        <div>
+            <h2> Title </h2>
+            <img> Author Avatar </img>
+            <h3> Author </h3>
+            <h3> Date </h3>
+        </div>
+    )
+}
+export default ArticleSummary
