db594bb69eefa2b7564a1f34b8c8728fcebab025
jdmedlock <jdmedlock@gmail.com>
2020-04-19 07:42:42 -0500

    Feature: Add base containers & components
    Add base containers & components to create a common codebase for Sprint 4
    
    Resolves: N/a
    See also: N/a

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,20 @@
 import React from 'react' 
-import './footer.css'
+import footerStyle from './footer.module.scss'
+
 const Footer = ()=> { 
   return ( 
-    <div className="container flex"> 
-      <ul className="flex"> 
-        <li className="menuList">About</li> 
-        <li className="menuList">Concept</li> 
-        <li className="menuList">Terms</li> 
-        <li className="menuList">Q&amp;A</li> 
-        <li className="menuList">Contact</li> 
+    <div className={ `footerStyle.container, footerStyle.flex` }> 
+      <ul className={ footerStyle.flex }> 
+        <li className={ footerStyle.menuList }>About</li> 
+        <li className={ footerStyle.menuList }>Concept</li> 
+        <li className={ footerStyle.menuList }>Terms</li> 
+        <li className={ footerStyle.menuList }>Q&amp;A</li> 
+        <li className={ footerStyle.menuList }>Contact</li> 
       </ul> 
-      <ul className="flex"> 
-        <li className="snsList"><i class="fab fa-twitter"></i></li> 
-        <li className="snsList"><i class="fab fa-github"></i></li> 
-        <li className="snsList"><i class="fab fa-facebook-square"></i></li> 
+      <ul className={ footerStyle.flex }> 
+        <li className={ footerStyle.snsList }><i className="fab fa-twitter"></i></li> 
+        <li className={ footerStyle.snsList }><i className="fab fa-github"></i></li> 
+        <li className={ footerStyle.snsList }><i className="fab fa-facebook-square"></i></li> 
       </ul> 
     </div> 
   ) 
