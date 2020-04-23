3083993e4717c1bab2a7b769c6734f035c4f23d6
Yuriko Darragh <beljar731@gmail.com>
2020-04-23 10:39:22 +0900

    updated slug temporaly

--- src/components/articleSummary.js
+++ src/components/articleSummary.js
@@ -1,33 +1,41 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 const ArticleSummary = ()=> {
 
-  const data = useStaticQuery(graphql`
-    query {
+const data = useStaticQuery(graphql`
+  query {
       allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              title
-              date
-            }
+          edges {
+              node {
+                  frontmatter {
+                      title
+                      date
+                  }
+                  fields {
+                      slug
+                  }
+              }
           }
-        }
       }
-    }
+  }
 `)
 
   return (
-    <ol>
-      {data.allMarkdownRemark.edges.map((edge) => {
-        return (
-          <li>
-            <h2>{edge.node.frontmatter.title}</h2>
-            <p>{edge.node.frontmatter.date}</p>
-          </li>
-        )
-    })}
-    </ol>
+    <div>
+      <h1>Article Summary</h1>
+        <ol>
+            {data.allMarkdownRemark.edges.map((edge) => {
+                return (
+                    <li>
+                        <Link to={`/article/${edge.node.fields.slug}`}>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </Link>
+                    </li>
+                )
+            })}
+        </ol>
+    </div>
   )
 }
 
