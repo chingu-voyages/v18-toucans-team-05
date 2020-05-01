4e02c0bfee65d697b663913fd96049d42ee087a7
Angel Young <angelyoung@Angels-iMac.local>
2020-05-01 10:53:02 -0400

    corrected changes for pull request

--- src/components/articleSummary.js
+++ src/components/articleSummary.js
@@ -27,18 +27,18 @@ const  ArticleSummary =()=> {
 
   return (
     <div className={ articleSummaryStyles.container}>
-      <h1 class={articleSummaryStyles.artTitle}>Articles</h1>
+      <h1 className={articleSummaryStyles.artTitle}>Articles</h1>
         <ol>
             {data.allMarkdownRemark.edges.map((edge) => {
                 return (
-                  <Link to={`/article/${edge.node.fields.slug}`}> 
+                  <Link to={`/article/${edge.node.fields.slug}`} key={`/article/${edge.node.fields.slug}`}> 
                     <div className={ articleSummaryStyles.articlebx }>
                       <li>
                         <h2>{edge.node.frontmatter.title}</h2>
-                        <div class={articleSummaryStyles.category}>&#35;{edge.node.frontmatter.category}</div>
-                        <div class={articleSummaryStyles.flex} >
-                            <div class={articleSummaryStyles.date}>{edge.node.frontmatter.date} &#47;</div>
-                            <div class={articleSummaryStyles.author}>{edge.node.frontmatter.author}</div>
+                        <div className={articleSummaryStyles.category}>&#35;{edge.node.frontmatter.category}</div>
+                        <div className={articleSummaryStyles.flex} >
+                            <div className={articleSummaryStyles.date}>{edge.node.frontmatter.date} &#47;</div>
+                            <div className={articleSummaryStyles.author}>{edge.node.frontmatter.author}</div>
                         </div>
                       </li>
                     </div>
