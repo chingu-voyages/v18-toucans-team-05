2b2bbb73f3103ac88b0259a984f07e48806ecfc2
dev-grrl-japan <22501219+dev-grrl-japan@users.noreply.github.com>
2020-05-10 17:23:17 +0900

    Added Search Components Folder and Pages.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import navbarStyle from '../styles/navbar.module.scss'
 import { Link } from 'gatsby'
+//import { Container, Logo } from './search/styles.js'
+import Search from './search'
 
-const Navbar = () => {
+const searchIndices = [
+  { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
+  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
+]
+
+const Navbar = ({ site, transparent }) => {
   return (
     <div className={navbarStyle.navcontainer}>
       <ul className={navbarStyle.articleList}>
@@ -28,6 +35,7 @@ const Navbar = () => {
         <li className={`navbarStyle.flex, navbarStyle.right`}><p><Link to="/javascript">#Javascript</Link></p></li>
         <li className={`navbarStyle.flex, navbarStyle.right`}><p><Link to="/css">#CSS</Link></p></li>
       </ul>
+      <Search collapse indices={searchIndices} />
     </div>
   )
 }
